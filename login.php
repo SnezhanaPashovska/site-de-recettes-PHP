a561911fd5ffdcacbe66a8c47792fe73cee121bd
Snezhana <92525161+SnezhanaPashovska@users.noreply.github.com>
2024-03-13 16:03:35 +0100

    Add files via upload

--- login.php
+++ login.php
@@ -31,7 +31,7 @@
 <?php else : ?>
 
   <div class="alert alert-success" role="alert">
-  Bonjour <?php echo $_SESSION['LOGGED_USER']; ?> et bienvenue sur le site !
+  Bonjour <?php echo $_SESSION['LOGGED_USER']['email']; ?> et bienvenue sur le site !
       
 	</div>
 
